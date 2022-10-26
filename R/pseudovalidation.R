fefdeff306709662dc064b90d57ef15a0178918f
Jasmin Ricard <jasminricard@hotmail.com>
2022-10-26 16:19:41 -0400

    Update pseudovalidation.R
    Adapt the pseudovalidation function to be multivariate.
    Adding a 'destandardize' parameter to offer the possibility of using the function without destandardizing betas with the test dataset standard deviations.

--- R/pseudovalidation.R
+++ R/pseudovalidation.R
@@ -1,8 +1,8 @@
 #' @title Performs `pseudovalidation' to select the best \eqn{\lambda} value in lassosum
 #' 
 #' @param bfile A plink bfile stem
-#' @param beta The matrix of estimated \eqn{\beta}s
-#' @param cor The vector of correlations (\eqn{r})
+#' @param beta The array of estimated \eqn{\beta}s where dimensions are, in order, SNPs, lambdas and traits.
+#' @param cor The matrix of correlations (\eqn{r}) where dimensions are, in order, SNPs and traits.
 #' @param sd The standard deviation of the SNPs
 #' @param extract SNPs to extract
 #' @param exclude SNPs to exclude
@@ -10,6 +10,7 @@
 #' @param remove samples to remove
 #' @param chr a vector of chromosomes
 #' @param cluster A \code{cluster} object from the \code{parallel} package for parallel computing
+#' @param destandardize should \eqn{\beta}s be destandardized using test dataset standard deviations before computing PRS?
 #' @details A function to calculate  
 #' \deqn{f(\lambda)=\beta'r/\sqrt{\beta'X'X\beta}} 
 #' where \eqn{X} is the standardized genotype matrix divided by \eqn{\sqrt n}, 
@@ -23,7 +24,7 @@
 #' @keywords internal
 pseudovalidation <- function(bfile, beta, cor, sd=NULL, 
                              keep=NULL, extract=NULL, exclude=NULL, remove=NULL, 
-                             chr=NULL, cluster=NULL, ...) {
+                             chr=NULL, cluster=NULL, destandardize = T, ...) {
 
   stopifnot(is.numeric(cor))
   stopifnot(!any(is.na(cor)))
@@ -34,30 +35,61 @@ pseudovalidation <- function(bfile, beta, cor, sd=NULL,
   stopifnot(!any(is.na(beta)))
   if(length(cor) != nrow(beta)) stop("Length of cor does not match number of rows in beta")
   
+  nbr_trait <- dim(beta)[3]
+  nbr_param <- dim(beta)[2]
   parsed <- parseselect(bfile, extract=extract, exclude = exclude, 
                         keep=keep, remove=remove, 
                         chr=chr)
-  if(length(cor) != parsed$p) stop("Length of cor does not match number of selected columns in bfile")
+  if(dim(beta)[1] != parsed$p) stop("The number of rows in beta does not match number of selected columns in bfile")
   
   
   if(is.null(sd)) sd <- sd.bfile(bfile = bfile, 
                                  keep=parsed$keep, extract=parsed$extract, ...)
   stopifnot(length(sd) == length(cor))
 
-    weight <- 1/sd
-    weight[!is.finite(weight)] <- 0
-    scaled.beta <- as.matrix(Diagonal(x=weight) %*% beta)
-    pred <- pgs(bfile, keep=parsed$keep, extract=parsed$extract, 
-                weights=scaled.beta, cluster=cluster)
-    pred2 <- scale(pred, scale=F)
-    bXXb <- colSums(pred2^2) / parsed$n
-    bXy <- cor %*% beta 
-    
-    result <- as.vector(bXy / sqrt(bXXb))
-    attr(result, "bXXb") <- bXXb
-    attr(result, "bXy") <- bXy
-    
-    return(result)
-    #' @return the results of the pseudovalidation, i.e. \eqn{f(\lambda)}
+  BETA <- as.vector(t(beta[,1,]))
+  r_hat <- as.vector(t(cor))
+  BETA <- data.frame(BETA)
+  if(nbr_param == 1){
+      warning("Data for only one parameter is found")
+  }else{
+      for(param in 2:nbr_param){
+          for(trait in 1:nbr_trait){
+              BETA_param <- as.vector(t(beta[,param,]))
+          }
+          BETA_param <- data.frame(BETA_param)
+          BETA <- cbind(BETA, BETA_param)
+      }
+  }
+  BETA <- as.matrix(BETA)
+  
+  weight <- 1/sd
+  weight[!is.finite(weight)] <- 0
+  
+  PRS <- array(rep(NA, parsed$n*nbr_param*nbr_trait),
+               dim = c(parsed$n, nbr_param, nbr_trait))
+  for(trait in 1:nbr_trait){
+      if(destandardize){
+          scaled_beta <- as.matrix(Diagonal(x = weight) %*% beta[,,trait])
+          PRS[,,trait] <- pgs(Data, keep=keep_sujets, weights = scaled_beta)
+      }else{
+          PRS[,,trait] <- pgs(Data, keep=keep_sujets, weights = beta[,,trait])
+      }
+  }
+  
+  PRED <- matrix(data = NA,nrow = nbr_trait*parsed$n, ncol = nbr_param)
+  for(param in 1:nbr_param){
+      PRED[,param] <- as.vector(t(PRS[,param,]))
+  }
+  pred2 <- scale(pred, scale=F)
+  bXXb <- colSums(pred2^2) / parsed$n
+  bXy <- cor %*% beta 
+  
+  result <- as.vector(bXy / sqrt(bXXb))
+  attr(result, "bXXb") <- bXXb
+  attr(result, "bXy") <- bXy
+  
+  return(result)
+  #' @return the results of the pseudovalidation, i.e. \eqn{f(\lambda)}
     
 }
